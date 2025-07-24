e15ff1bdba6686f3917351283cea41939ebf485d
ElSayed ElSheikh <elsayed.elsheikh97@gmail.com>
2025-07-24 21:10:04 +0300

    Update

--- .github/actions/test/run.sh
+++ .github/actions/test/run.sh
@@ -8,13 +8,8 @@ rosdep update --rosdistro=humble
 rosdep install --from-paths . --ignore-src -y --rosdistro=humble 
 
 # Build
-BUILD_TYPE=RelWithDebInfo
-colcon build \
-        --merge-install \
-        --symlink-install \
-        --cmake-args "-DCMAKE_BUILD_TYPE=$BUILD_TYPE" "-DCMAKE_EXPORT_COMPILE_COMMANDS=On" \
-        -Wall -Wextra -Wpedantic
+colcon build --symlink-install -Wall -Wextra -Wpedantic
 
 # Test
 if [ -f install/setup.bash ]; then source install/setup.bash; fi
-colcon test --merge-install
+colcon test
