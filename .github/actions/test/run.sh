f750a5d2f81ec601cea1106da25c31014674dd86
ElSayed ElSheikh <elsayed.elsheikh97@gmail.com>
2025-07-24 21:14:18 +0300

    Update

--- .github/actions/test/run.sh
+++ .github/actions/test/run.sh
@@ -8,7 +8,7 @@ rosdep update --rosdistro=humble
 rosdep install --from-paths . --ignore-src -y --rosdistro=humble 
 
 # Build
-colcon build --symlink-install -Wall -Wextra -Wpedantic
+colcon build --symlink-install
 
 # Test
 if [ -f install/setup.bash ]; then source install/setup.bash; fi
