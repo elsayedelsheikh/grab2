4af1b1acc6c2e881d7355e6b98bf74775490796a
zaynab ahmad <zozaahmad203@gmail.com>
2025-10-18 12:19:24 +0300

    sinle thread with no time optimization

--- build/behaviortree_ros2/colcon_command_prefix_build.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/zaynap/grab/grab2/install/btcpp_ros2_interfaces/share/btcpp_ros2_interfaces/package.sh"
--- install/grab2_msgs/share/grab2_msgs/cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(grab2_msgs_IDL_FILES "action/ComputePlanToPose.idl;action/ComputePlanThroughPoses.idl;action/GetIsaacGrasp.idl")
-set(grab2_msgs_INTERFACE_FILES "action/ComputePlanToPose.action;action/ComputePlanThroughPoses.action;action/GetIsaacGrasp.action")
