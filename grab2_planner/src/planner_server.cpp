ca23150ced884e0e6e2ef90b01b0a9f5656fd5a9
zaynab ahmad <zozaahmad203@gmail.com>
2025-11-23 21:48:12 +0200

    adding changing

--- grab2_planner/src/planner_server.cpp
+++ grab2_planner/src/planner_server.cpp
@@ -31,10 +31,21 @@ PlannerServer::initialize()
     this->get_parameter_or("planning_group", planning_group_, std::string("panda_arm"));
 
     // Create MoveGroupInterface
+  #if RCLCPP_VERSION_GTE(28, 0, 0)
+    
+    move_group_interface_ =
+      std::make_unique<moveit::planning_interface::MoveGroupInterface>(
+        this->shared_from_this(),
+        planning_group_,
+        "robot_description");
+  #else
+    
     move_group_interface_ =
       std::make_unique<moveit::planning_interface::MoveGroupInterface>(
         this->shared_from_this(),
         planning_group_);
+  #endif
+
 
     initialized_ = true;
     RCLCPP_INFO(this->get_logger(), "PlannerServer initialized with planning group: %s",
